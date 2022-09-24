5a94ba6e4caa33f18b5dc36205b86cde681dbff7
Josue Torres <josuetolvera@gmail.com>
2022-09-23 20:06:41 -0500

    solve all the problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,63 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let totalAdd = num1 + num2;
+  return totalAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let totalSubtract = num1 - num2;
+  return totalSubtract;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// let totalSum = 0;
+  // for (let i = 0; i < array.length; i++) {
+  //   totalSum += array[i];
+  // }
+
+  const totalSum = array.reduce((total, currentValue) => {
+    return total + currentValue}, 0);
+
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const totalMultiply = array.reduce((total, currentValue) => {
+    return total * currentValue
+  });
+  return totalMultiply;
 };
 
-const power = function() {
-	
+const power = function(integer, power) {
+	const total = Math.pow(integer, power); // integer ** power produces the same result.
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	// if (num === 0){
+  //   return 1;
+  // } else {
+  //   return (num * factorial(num - 1))
+  // }
+
+  // let result = num;
+  // if (num === 0 || num === 1) {
+  //   return 1;
+  // }
+
+  // while (num > 1) {
+  //   num --;
+  //   result = result * num;
+  // }
+  // return result;
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
